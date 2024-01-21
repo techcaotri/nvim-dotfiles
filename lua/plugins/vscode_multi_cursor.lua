8ddb6e1ed320d70790e58b43ad8262acabd23ff2
Tri Pham <techcaotri@gmail.com>
2024-01-21 12:17:11 +0700

    Add 'vscode-multi-cursor' plugin and related configs

---
+++ lua/plugins/vscode_multi_cursor.lua
@@ -0,0 +1,13 @@
+return {
+  'vscode-neovim/vscode-multi-cursor.nvim',
+  event = 'VeryLazy',
+  vscode = true,
+  cond = not not vim.g.vscode,
+  opts = {},
+  config = function()
+    vim.keymap.set({ "n", "x", "i" }, "<C-n>", function()
+      require("vscode-multi-cursor").addSelectionToNextFindMatch()
+    end)
+
+    require('vscode-multi-cursor').setup()
+
