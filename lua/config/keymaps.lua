7007f100b1c7aa6c39edb64c341c4c63989bafed
Tri Pham <techcaotri@gmail.com>
2024-02-21 09:59:06 +0700

    ignore 'y' keymap for nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving dow
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half-page" })
 vim.keymap.set("n", "<c-a-n>", "<Plug>(YankyCycleForward)")
 vim.keymap.set("n", "<c-a-p>", "<Plug>(YankyCycleBackward)")
-vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
+if not vim.g.vscode then
+  vim.keymap.set({ "n", "x" }, "y", "<Plug>(YankyYank)")
+end
 vim.keymap.set("n", "=p", "<Plug>(YankyPutAfterFilter)")
 vim.keymap.set("n", "=P", "<Plug>(YankyPutBeforeFilter)")
