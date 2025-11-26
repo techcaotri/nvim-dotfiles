de3f78e92d77d10cbfa65aed41d0067154b08ca3
Tri Pham <v.tripc2@vinai.io>
2025-11-26 16:39:39 +0700

    Remove image support and add `nvim-java` plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,36 @@
 -- image.nvim
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+
+if not vim.g.vscode then
+  require('java').setup({
+    root_markers = {
+      'settings.gradle',
+      'settings.gradle.kts',
+      'pom.xml',
+      'build.gradle',
+      'mvnw',
+      'gradlew',
+      'build.gradle',
+      'build.gradle.kts'
+    },
+    jdtls = {
+      version = 'v1.52.0',
+    }
+  })
+
+  require('lspconfig').jdtls.setup {
+    -- ... other jdtls configurations ...
+    root_dir = require('lspconfig.util').root_pattern(
+      'settings.gradle',
+      'settings.gradle.kts',
+      'pom.xml',
+      'build.gradle',
+      'mvnw',
+      'gradlew',
+      'build.gradle',
+      'build.gradle.kts'
+    ),
+    -- ...
+  }
+end
